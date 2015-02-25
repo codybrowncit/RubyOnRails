828c8a43ec997b2abb2a71a3384fe435380ff5bc
Cody Brown <codybrowncit@gmail.com>
2015-02-25 16:58:07 -0700

    changed users in order controller

--- sexy_calculator/app/controllers/orders_controller.rb
+++ sexy_calculator/app/controllers/orders_controller.rb
@@ -4,9 +4,10 @@ class OrdersController < ApplicationController
 	end
 	def create
 		# get data from the form
-		order_params = params.require(:order).permit(:user_id, :width, :depth, :height, :distance, :additional_slack, :starting_u, :ending_u, :racks_crossed, :racks_through, :aisles)
+		order_params = params.require(:order).permit(:width, :depth, :height, :distance, :additional_slack, :starting_u, :ending_u, :racks_crossed, :racks_through, :aisles)
 		#give the data to the model
 		@order = Order.new(order_params)
+		@order.user = current_user
 		#save the model
 		if @order.save
 			# redirect to show or index
@@ -20,7 +21,7 @@ class OrdersController < ApplicationController
 	end
 	def update
 		# get data from the form
-		order_params = params.require(:order).permit(:user_id, :width, :depth, :height, :distance, :additional_slack, :starting_u, :ending_u, :racks_crossed, :racks_through, :aisles)
+		order_params = params.require(:order).permit(:width, :depth, :height, :distance, :additional_slack, :starting_u, :ending_u, :racks_crossed, :racks_through, :aisles)
 		#give the data to the model
 		@order = Order.find(params[:id])
 		#save the model
