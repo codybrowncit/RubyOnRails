3c1081fbe434963f02ad2a4dfa627d5c693d930c
Cody Brown <codybrowncit@gmail.com>
2015-03-02 16:45:16 -0700

    cancan updated code

--- traditions_book/app/controllers/application_controller.rb
+++ traditions_book/app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :user_signed_in?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
   private
 
   def authenticate_user!
